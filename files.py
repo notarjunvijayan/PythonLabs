9ac7697c57d1720266f8637db71ea7a3bd09f5d6
Arjun <vijayanarjun4@gmail.com>
2022-02-16 21:10:35 +0530

    files.py now working!

--- files.py
+++ files.py
@@ -6,5 +6,18 @@ try:
 except FileExistsError:
     os.chdir("Files/")
 print(f"Current working Directory {os.getcwd()}")
-f = open("files.txt" "r")
+file = open("files.txt", "a")
+if file:
+    print("File created/opened successfully")
+string = input("Enter the input for the string: ")
+file.write(string)
+print("Entered string written successfully: ")
+file.close()
+file = open("files.txt", "r")
+filetxt = file.read()
+print(filetxt)
+
+
+
+
 
